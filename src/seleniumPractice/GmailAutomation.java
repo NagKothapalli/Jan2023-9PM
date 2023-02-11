bed1699bbe3ac57d25e2e1db993d8ad99666218a
NagKothapalli <nag022@gmail.com>
2023-02-11 09:09:20 +0530

    Feb10th code pushed

--- src/seleniumPractice/GmailAutomation.java
+++ src/seleniumPractice/GmailAutomation.java
@@ -1,8 +1,12 @@
 package seleniumPractice;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import junit.framework.Assert;
@@ -12,13 +16,20 @@ public class GmailAutomation
 	//org.openqa.selenium.SessionNotCreatedException : Find the reason below	
 		//Could not start a new session. Response code 500. Message: session not created: This version of ChromeDriver only supports Chrome version 110
 	    //Current browser version is 90.0.4430.212 with binary path C:\Program Files (x86)\Google\Chrome\Application\chrome.exe 
+	//org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {"method":"css selector","selector":"*[name='identifier']"}
+	WebDriver driver;//null
+	public GmailAutomation()
+	{
+		System.out.println("Constructor of GmailAutomation");
+		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
+		driver = new ChromeDriver(); //1234
+	}
 	String expectedTitle = "Amail";
-	@Test
+	@Before
 	public void launchApplication()
 	{
 		System.out.println("Test Case : Launch Appliction");
-		System.setProperty("webdriver.chrome.driver", "D:\\Softwares\\JarFiles\\chromedriver-win32-90\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();  //5 mnts pls 
+		 //5 mnts pls 
 		//Inside the constructor of ChromeDriver class they have written a code to open an empty 
 		 // google chrome browser
 		driver.get("https://gmail.com");
@@ -37,7 +48,36 @@ public class GmailAutomation
 			System.out.println("Title Vlidation Failed");
 		}
 		System.out.println("Code after if condition");
+		
+	}
+	//TestMethod Execution -> Constructor -> Before   ->  TestMethod
+	@Test
+	public void loginToApplication()
+	{
+		System.out.println("Test Case : Login to Application");
 		driver.findElement(By.name("identifier")).sendKeys("nag022");
+		WebElement emailObj = driver.findElement(By.name("identifier"));
+		emailObj.click();
+		emailObj.clear();
+		emailObj.sendKeys("nag022@gmail.com");
+		emailObj.clear();
+		emailObj.sendKeys("9959775757");
+		List<WebElement> elements = driver.findElements(By.className("VfPpkd-vQzf8d"));
+		WebElement element = elements.get(1); // 0 1 2 3
+		element.click();
+		//driver.findElement(By.className("VfPpkd-Jh9lGc")).click();
+		//driver.findElement(By.linkText("Try again")).click();
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
